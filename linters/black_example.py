df1fd435c65e6ba06d2edbd1046f8f16ee64277d
Your Name <you@example.com>
2025-05-25 10:37:37 +0300

    Added black example

---
+++ linters/black_example.py
@@ -0,0 +1,12 @@
+import os,sys,math
+def  add(x,y):return   x+y
+def multiply(a,b):
+ return a*b
+def long_function_name(arg1,arg2,arg3,arg4,arg5,arg6,arg7,arg8,arg9):return arg1+arg2+arg3+arg4+arg5+arg6+arg7+arg8+arg9
+def foo():print( 'foo' )
+def bar(): print ( "bar" )
+def baz():
+  print("baz" )
+
+x=add(1,2);y=multiply(3,4);z=long_function_name(1,2,3,4,5,6,7,8,9)
+if x>0:print(  'positive')else:print(   'negative'   )
